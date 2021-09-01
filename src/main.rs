5fa22f6e4a5e9a48cd6bacfa1d9259154127b2ec
devguchi <devguchitaro@gmail.com>
2021-09-01 13:37:05 +0900

    added hello

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 fn main() {
     println!("Hello, world!");
-    println!("{}", calc(12, 23))
+    println!("{}", calc(12, 23));
+    println!("{}", hello("hoge"));
 }
 
 fn calc(x: i64, y:i64) -> i64 {
     x + y
+}
+
+fn hello(s: &str) -> String {
+    println!("{}", s);
+    let s2: String = s.to_string();
+    println!("{}", s2);
+    s2+"HOGE"
 }
