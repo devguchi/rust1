f10fa6425938d776040016de0558583dfe280d40
devguchi <devguchitaro@gmail.com>
2021-09-09 23:04:10 +0900

    added mcro

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,15 @@ fn main() {
         }
     };
     println!("{}", b);
+    let s = format!("{} {}", "1", "2");
+    println!("{}", s);
+    print!("hoge");
+    print!("hoge2");
+    eprint!("error {}", "ooo");
+    println!("hoge2");
+    let s = [1,2];
+    dbg!(s);
+    println!("file is {}", file!());
+    println!("line is {}", line!());
 }
 
