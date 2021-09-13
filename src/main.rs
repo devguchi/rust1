860b5efbd179f10f5421160f5a29ccc2786d813c
devguchi <devguchitaro@gmail.com>
2021-09-13 20:34:45 +0900

    added arc, mutex

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
 use std::thread;
+use std::sync::{Arc, Mutex};
 
 fn main() {
-    let mut a = 100;
-    thread::spawn(move ||{
-        println!("hoge");
-        for x in 0..100 {
-            println!("hoge {} {}", x, a);
-        }
-    });
-    a = 200;
-    let handle = thread::spawn(move ||{
-        println!("hoge2");
-        for x in 0..10 {
-            a += x;
-            println!("hoge2 {} {}", x, a);
-        }
-    });
-    let _ = handle.join();
-    println!("hoge3 {}", a);
+    let mut handles = Vec::new();
+    let hoge = Arc::new(Mutex::new(vec![1; 10]));
+    let hoge2 = Arc::new(Mutex::new(0));
+    for x in 0..10 {
+        let data_ref = hoge.clone();
+        let data2_ref = hoge2.clone();
+        let handle = thread::spawn(move ||{
+            let mut data = data_ref.lock().unwrap();
+            let mut data2 = data2_ref.lock().unwrap();
+            data[x] = x;
+            *data2 += x;
+            println!("thread {} {} {}", x, data[x], data2);
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    dbg!(hoge);
+    dbg!(hoge2);
 }
 
 
