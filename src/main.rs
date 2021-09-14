1843fec9a3139db5c2b8d0a0f1fb2d86b533d4c5
devguchi <devguchitaro@gmail.com>
2021-09-15 01:16:37 +0900

    added Result

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use std:: env;
+use std::fs::File;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let num1 = args[1].parse::<u32>().unwrap();
-    let num2 = args[2].parse::<u32>().unwrap();
-    println!("num1 + num2 = {}", num1 + num2);
+    let file_name = args[1].to_string();
+    println!("file: {}", file_name);
+    let f = File::open(file_name);
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("ERROR! {:?}", error);
+        },
+    };
+    dbg!(f);
 }
+
 
