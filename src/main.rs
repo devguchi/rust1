4cf07138c6fb1ae3a4fe3f86f16648650c541eb9
devguchi <devguchitaro@gmail.com>
2021-09-15 19:11:45 +0900

    atcoder - ABC083B

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 use std::io;
 
 fn main() {
-    let mut words = get_input();
-    let a: i32 = words[0].parse().unwrap();
-    words = get_input();
-    let b: i32 = words[0].parse().unwrap();
-    let c: i32 = words[1].parse().unwrap();
-    words = get_input();
-    let s = &words[0];
-    let sum = a + b + c;
-    println!("{} {}", sum, s);
+    let a_max:u32 = get_input()[0].parse().unwrap();
+    let b_max:u32 = get_input()[0].parse().unwrap();
+    let c_max:u32 = get_input()[0].parse().unwrap();
+    let mut count = 0;
+    let x:u32 = get_input()[0].parse().unwrap();
+    for a in 0..a_max+1 {
+        if x < 500*a {
+            break;
+        }
+        for b in 0..b_max+1 {
+            if x < 500*a+100*b {
+                break;
+            }
+            let c = (x-500*a-100*b)/50;
+            if c <= c_max {
+                count += 1;
+            }
+        }
+    }
+    println!("{}", count);
 }
 
 fn get_input() -> Vec<String> {
@@ -18,5 +29,4 @@ fn get_input() -> Vec<String> {
     let words: Vec<&str> = word_line.split_whitespace().collect();
     words.iter().map(|word| word.to_string()).collect()
 }
-
 
