8cb57b3b122356243866b1f0a665412fad3001a2
devguchi <devguchitaro@gmail.com>
2021-09-15 15:37:30 +0900

    added get_input & atcoder practiceA

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
-use std:: env;
-use std::fs::File;
+use std::io;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let file_name = args[1].to_string();
-    println!("file: {}", file_name);
-    let f = File::open(file_name);
-    let f = match f {
-        Ok(file) => file,
-        Err(error) => {
-            panic!("ERROR! {:?}", error);
-        },
-    };
-    dbg!(f);
+    let mut words = get_input();
+    let a: i32 = words[0].parse().unwrap();
+    words = get_input();
+    let b: i32 = words[0].parse().unwrap();
+    let c: i32 = words[1].parse().unwrap();
+    words = get_input();
+    let s = &words[0];
+    let sum = a + b + c;
+    println!("{} {}", sum, s);
+}
+
+fn get_input() -> Vec<String> {
+    let mut word_line = String::new();
+    io::stdin().read_line(&mut word_line).ok();
+    let words: Vec<&str> = word_line.split_whitespace().collect();
+    words.iter().map(|word| word.to_string()).collect()
 }
 
 
