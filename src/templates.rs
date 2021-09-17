94e6f76462f1ba1d850bcaceca66bb11746f5b03
devguchi <devguchitaro@gmail.com>
2021-09-17 09:55:29 +0900

    added templates.rs

---
+++ src/templates.rs
@@ -0,0 +1,18 @@
+use std::io;
+
+fn get_input() -> Vec<String> {
+    let mut word_line = String::new();
+    io::stdin().read_line(&mut word_line).ok();
+    let words: Vec<&str> = word_line.split_whitespace().collect();
+    words.iter().map(|word| word.to_string()).collect()
+}
+
+fn get_input_lines(line_len:u32) -> Vec<String> {
+    let mut vec:Vec<String> = vec![];
+    let mut input:Vec<String>;
+    for _ in 0..line_len {
+        input = get_input();
+        vec.append(&mut input);
+    }
+    vec
+}
