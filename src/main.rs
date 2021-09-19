b56ef9622b2ed8b7808afe1e32e113bd5a0af90d
devguchi <devguchitaro@gmail.com>
2021-09-19 19:35:36 +0900

    atcoder - abc218-a

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,13 @@
 use std::io;
 
 fn main() {
+    let n:usize = get_input()[0].parse().unwrap();
     let s = &get_input()[0];
-    let s_vec:Vec<char> = s.chars().rev().collect();
-    let targets = ["dream", "dreamer", "erase", "eraser"];
-    let mut targets_rev:Vec<String> = vec![]; 
-    for target in &targets {
-        targets_rev.push(target.to_string().chars().rev().collect());
-    }
-    let mut i = 0;
-    'outer:loop {
-        if i > s_vec.len()-1 {
-            println!("YES");
-            std::process::exit(0);
-        }
-        let mut s_str:String;
-        for target in &targets_rev {
-            if i+target.len() > s_vec.len() {
-                continue;
-            }
-            s_str = s_vec[i..i+target.len()].into_iter().collect();
-            if s_str == *target {
-                i += target.len();
-                continue 'outer;
-            }
-        }
-        println!("NO");
-        std::process::exit(0);
+    let s_vec:Vec<char> = s.chars().collect();
+    if s_vec[n-1] == 'o' {
+        println!("Yes");
+    } else {
+        println!("No");
     }
 }
 
