da26914f529ae3321462956c62ca5e85a1d7918a
devguchi <devguchitaro@gmail.com>
2021-09-21 09:42:23 +0900

    atcoder - abc211-a

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,10 @@ use std::io;
 
 fn main() {
     let numbers:Vec<i64> = get_input_i64();
-    let a = numbers[0];
-    let b = numbers[1];
-    if 0 < a && b == 0 {
-        println!("Gold");
-    } else if a == 0 && 0 < b {
-        println!("Silver");
-    } else if 0 < a && 0 < b {
-        println!("Alloy");
-    }
+    let a = numbers[0] as f64;
+    let b = numbers[1] as f64;
+    let c = (a-b)/3.0+b;
+    println!("{}",c);
 }
 
 fn get_input() -> Vec<String> {
