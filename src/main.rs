6d1ab587206101dadc308865115aaf6c62e5c79b
devguchi <devguchitaro@gmail.com>
2021-09-22 10:44:34 +0900

    atcoder - abc208-a

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
-use textplots::{Chart, Plot, Shape};
+use std::io;
 
 fn main() {
-    println!("y = sin(x) / x");
-    Chart::default()
-        .lineplot(&Shape::Continuous(Box::new(|x| x.sin() / x)))
-        .display();
+    let numbers = get_input_i64();
+    let a = numbers[0] as f64;
+    let b = numbers[1] as f64;
+    if b/6.0 <= a && a <= b {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
+
+fn get_input() -> Vec<String> {
+    let mut word_line = String::new();
+    io::stdin().read_line(&mut word_line).ok();
+    let words: Vec<&str> = word_line.split_whitespace().collect();
+    words.iter().map(|word| word.to_string()).collect()
+}
+
+fn get_input_i64() -> Vec<i64> {
+    let words = get_input();
+    words.iter().map(|word| word.parse().unwrap()).collect()
+}
+
