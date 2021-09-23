3b3a3baade519d42a262982a81fd349890397e8a
devguchi <devguchitaro@gmail.com>
2021-09-23 18:21:31 +0900

    atcoder - abc200-a

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
 use std::io;
 
 fn main() {
-    let mut numbers = get_input_i64();
-    numbers.sort();
-    let a1 = numbers[0];
-    let a2 = numbers[1];
-    let a3 = numbers[2];
-    if a2 - a1 == a3 - a2 {
-        println!("Yes");
-    } else {
-        println!("No");
-    }
+    let numbers = get_input_i64();
+    let n = numbers[0] as f64;
+    let ans = (n/100.0).ceil();
+    println!("{}", ans);
 }
 
 fn get_input() -> Vec<String> {
