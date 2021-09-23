86528b32e656a9eda77367ebfc3913028802a6cd
devguchi <devguchitaro@gmail.com>
2021-09-23 19:54:00 +0900

    atcoder - abc193-a

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,10 @@ use std::io;
 
 fn main() {
     let ab = get_input_i64();
-    let a = ab[0];
-    let b = ab[1];
-    let ab = a+b;
-    if ab >= 15 && b >= 8 {
-        println!("1");
-    } else if ab >= 10 && b >= 3 {
-        println!("2");
-    } else if ab >= 3 {
-        println!("3");
-    } else {
-        println!("4");
-    }
+    let a = ab[0] as f64;
+    let b = ab[1] as f64;
+    let ans = (a-b)/a*100.0;
+    println!("{}", ans);
 }
 
 fn get_input() -> Vec<String> {
