c1b658ca0881347fc0d8edf8f126277b3b3d2251
devguchi <devguchitaro@gmail.com>
2021-09-23 16:58:46 +0900

    atcoder - abc201-a

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::io;
 
 fn main() {
-    let numbers = get_input_i64();
-    let a = numbers[0];
-    let b = numbers[1];
-    let c = numbers[2];
-    let ans = (7-a)+(7-b)+(7-c);
-    println!("{}", ans);
+    let mut numbers = get_input_i64();
+    numbers.sort();
+    let a1 = numbers[0];
+    let a2 = numbers[1];
+    let a3 = numbers[2];
+    if a2 - a1 == a3 - a2 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
 
 fn get_input() -> Vec<String> {
