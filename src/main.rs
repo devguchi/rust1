1b03c84b68cd4db90eb2255134eb3b84f38024c4
devguchi <devguchitaro@gmail.com>
2021-09-26 10:28:28 +0900

    atcoder - abc213-b

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
 use std::io;
 
 fn main() {
-    let n = get_input_i64();
-    let s = n[0];
-    let t = n[1];
-    let mut ans = 0;
-    for a in 0..101 {
-        for b in 0..101 {
-            for c in 0..101 {
-                if a+b+c <= s && a*b*c <= t {
-                    ans += 1;
-                }
-            }
-        }
-    }
-    println!("{}", ans);
+    let _ = get_input_i64()[0];
+    let a = get_input_i64();
+    let mut a2 = a.clone();
+    a2.sort_by(|a,b| b.cmp(a));
+    let score = a2[1];
+    let idx = a.iter().position(|&x| x == score).unwrap();
+    println!("{}", idx+1);
 }
 
 fn get_input() -> Vec<String> {
@@ -28,3 +21,4 @@ fn get_input_i64() -> Vec<i64> {
     let words = get_input();
     words.iter().map(|word| word.parse().unwrap()).collect()
 }
+
