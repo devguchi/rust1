21d142395cc8a4df8480da50248a9e38e5b99398
devguchi <devguchitaro@gmail.com>
2021-09-26 11:46:40 +0900

    atcoder - abc211-b

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,13 @@ use std::io;
 use std::collections::HashSet;
 
 fn main() {
-    let s = &get_input()[0];
-    let v = string_vec_u32(&s);
-    let h:HashSet<u32> = v.clone().into_iter().collect();
-    if h.len() < 2 {
-        println!("Weak");
-        std::process::exit(0);
+    let w = get_input_lines(4);
+    let h:HashSet<String> = w.into_iter().collect();
+    if h.len() == 4 {
+        println!("Yes");
+    } else {
+        println!("No");
     }
-    let next: [u32; 10] = [1,2,3,4,5,6,7,8,9,0];
-    let mut prev: usize = 100;
-    for i in v.iter() {
-        if prev < 10 && *i != next[prev] {
-            println!("Strong");
-            std::process::exit(0);
-        }
-        prev = *i as usize;
-    }
-    println!("Weak");
 }
 
 fn get_input() -> Vec<String> {
@@ -28,7 +18,12 @@ fn get_input() -> Vec<String> {
     words.iter().map(|word| word.to_string()).collect()
 }
 
-fn string_vec_u32(s:&String) -> Vec<u32> {
-    s.chars().map(|c| c.to_digit(10).unwrap()).collect()
+fn get_input_lines(line_len:u32) -> Vec<String> {
+    let mut vec:Vec<String> = vec![];
+    let mut input:Vec<String>;
+    for _ in 0..line_len {
+        input = get_input();
+        vec.append(&mut input);
+    }
+    vec
 }
-
