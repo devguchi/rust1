ceea1b94ad73323cdb3a892ea7da524841202608
devguchi <devguchitaro@gmail.com>
2021-09-27 12:24:07 +0900

    atcoder - abc210-b

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::io;
-use std::collections::HashSet;
 
 fn main() {
-    let w = get_input_lines(4);
-    let h:HashSet<String> = w.into_iter().collect();
-    if h.len() == 4 {
-        println!("Yes");
-    } else {
-        println!("No");
+    let _ = get_input_i64()[0];
+    let s = &get_input()[0];
+    let vc:Vec<char> = s.chars().collect();
+    let idx = vc.iter().position(|&x| x == '1').unwrap();
+    let mut loser = "Takahashi";
+    if idx % 2 == 1 {
+        loser = "Aoki";
     }
+    println!("{}", loser);
 }
 
 fn get_input() -> Vec<String> {
@@ -18,12 +19,8 @@ fn get_input() -> Vec<String> {
     words.iter().map(|word| word.to_string()).collect()
 }
 
-fn get_input_lines(line_len:u32) -> Vec<String> {
-    let mut vec:Vec<String> = vec![];
-    let mut input:Vec<String>;
-    for _ in 0..line_len {
-        input = get_input();
-        vec.append(&mut input);
-    }
-    vec
+fn get_input_i64() -> Vec<i64> {
+    let words = get_input();
+    words.iter().map(|word| word.parse().unwrap()).collect()
 }
+
