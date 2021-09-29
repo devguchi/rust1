da05ddbe67303d97983da2564f3ee790bc0b6bf0
devguchi <devguchitaro@gmail.com>
2021-09-29 15:03:13 +0900

    atcoder - dp_a

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use std::io;
-use std::collections::HashSet;
 
 fn main() {
-    let n = get_input_i64()[0];
-    let a = get_input_i64();
-    let ah:HashSet<i64> = a.into_iter().collect();
-    if ah.len() == n as usize {
-        println!("Yes");
-    } else {
-        println!("No");
+    let n = get_input_i64()[0] as usize;
+    let h = get_input_i64();
+    let mut score:Vec<i64> = vec![0];
+    for i in 1..n {
+        let idx = i as usize;
+        if i == 1 {
+            score.push((h[idx]-h[idx-1]).abs());
+        } else {
+            let s1 = (h[idx]-h[idx-1]).abs()+score[idx-1];
+            let s2 = (h[idx]-h[idx-2]).abs()+score[idx-2];
+            if s1 < s2 {
+                score.push(s1);
+            } else {
+                score.push(s2);
+            }
+        }
     }
+    println!("{}", score[n-1]);
 }
 
 fn get_input() -> Vec<String> {
