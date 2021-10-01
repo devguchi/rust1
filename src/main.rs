6beea8cf047fb1d1011628ee0b6464deb2615b36
devguchi <devguchitaro@gmail.com>
2021-10-01 16:52:37 +0900

    atcoder - abc202-b

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 use std::io;
 
 fn main() {
-    let nk = get_input_i64();
-    let n = nk[0];
-    let k = nk[1];
-    let mut total = 0;
-    for i in 1..n+1 {
-        for j in 1..k+1 {
-            total += 100*i+j;
+    let s = &get_input()[0];
+    let sv:Vec<char> = s.chars().rev().collect();
+    for i in sv.iter() {
+        if *i == '6' {
+            print!("9");
+        } else if *i == '9' {
+            print!("6");
+        } else {
+            print!("{}", i);
         }
     }
-    println!("{}", total);
 }
 
-fn get_input_i64() -> Vec<i64> {
+fn get_input() -> Vec<String> {
     let mut word_line = String::new();
     io::stdin().read_line(&mut word_line).ok();
     let words: Vec<&str> = word_line.split_whitespace().collect();
-    words.iter().map(|word| word.parse().unwrap()).collect()
+    words.iter().map(|word| word.to_string()).collect()
 }
 
