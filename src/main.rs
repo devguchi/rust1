d9b3e4e975bfdfa2f393ea87d66ca0e0a542ef60
devguchi <devguchitaro@gmail.com>
2021-10-01 16:18:43 +0900

    atcoder - abc185-a

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,11 @@
 use std::io;
 use std::cmp::min;
 
-
 fn main() {
-    let n = get_input_i64()[0] as usize;
-    let mut dp:[i64; 100010] = [std::i64::MAX; 100010];
-    dp[0] = 0;
-    for i in 1..n+1 {
-        let iu = i as usize;
-        let mut pow6 = 1;
-        let mut pow9 = 1;
-        while pow6 <= i {
-            dp[iu] = min(dp[iu], dp[iu-pow6]+1);
-            pow6 *= 6;
-        }
-        while pow9 <= i {
-            dp[iu] = min(dp[iu], dp[iu-pow9]+1);
-            pow9 *= 9;
-        }
-    }
-    println!("{}", dp[n]);
+    let a = get_input_i64();
+    let min12 = min(a[0], a[1]);
+    let min34 = min(a[2], a[3]);
+    println!("{}", min(min12, min34));
 }
 
 fn get_input_i64() -> Vec<i64> {
