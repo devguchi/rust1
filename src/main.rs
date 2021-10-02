50299a144ce4317b5a0ca2b757ba8ebd71af388b
devguchi <devguchitaro@gmail.com>
2021-10-02 21:39:19 +0900

    atcoder - abc183-a

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,8 @@
 use std::io;
 
-struct Mountain {
-    name: String,
-    height: i64
-}
-
 fn main() {
-    let n = get_input()[0].parse().unwrap();
-    let mut mountains: Vec<Mountain> = vec![];
-    for _ in 0..n {
-        let m = get_input();
-        mountains.push(Mountain {
-            name: m[0].clone(),
-            height: m[1].parse().unwrap()
-        });
-    }
-    mountains.sort_by(|a,b| b.height.cmp(&a.height));
-    println!("{}", mountains[1].name);
+    let x = get_input()[0].parse().unwrap();
+    println!("{}", if x >= 0 { x } else { 0 });
 }
 
 fn get_input() -> Vec<String> {
@@ -25,4 +11,5 @@ fn get_input() -> Vec<String> {
     let words: Vec<&str> = word_line.split_whitespace().collect();
     words.iter().map(|word| word.to_string()).collect()
 }
+
 
