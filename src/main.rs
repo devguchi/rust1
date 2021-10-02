dc29568a829fc38816ff92afccd2c5f5643b7655
devguchi <devguchitaro@gmail.com>
2021-10-02 21:53:41 +0900

    atcoder - abc180-a

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use std::io;
 
 fn main() {
-    let n:i64 = get_input()[0].parse().unwrap();
-    println!("{}", if n%2 == 1 { "Black" } else { "White" });
+    let nab = get_input_i64();
+    let n = nab[0];
+    let a = nab[1];
+    let b = nab[2];
+    println!("{}", n-a+b);
 }
 
-fn get_input() -> Vec<String> {
+fn get_input_i64() -> Vec<i64> {
     let mut word_line = String::new();
     io::stdin().read_line(&mut word_line).ok();
     let words: Vec<&str> = word_line.split_whitespace().collect();
-    words.iter().map(|word| word.to_string()).collect()
+    words.iter().map(|word| word.parse().unwrap()).collect()
 }
-
 
