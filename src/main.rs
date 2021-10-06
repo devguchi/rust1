ae8aa85fef981d96d9d7cb9e1bad60436bcf39c5
devguchi <devguchitaro@gmail.com>
2021-10-06 20:50:20 +0900

    atcoder - abc177-a

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use std::io;
 
 fn main() {
-    let s:i64 = get_input()[0].parse().unwrap();
-    println!("{}", if s == 0 { 1 } else { 0 });
+    let dts = get_input_i64();
+    let d = dts[0] as f64;
+    let t = dts[1] as f64;
+    let s = dts[2] as f64;
+    if d/s <= t {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
 
-fn get_input() -> Vec<String> {
+fn get_input_i64() -> Vec<i64> {
     let mut word_line = String::new();
     io::stdin().read_line(&mut word_line).ok();
     let words: Vec<&str> = word_line.split_whitespace().collect();
-    words.iter().map(|word| word.to_string()).collect()
+    words.iter().map(|word| word.parse().unwrap()).collect()
 }
-
