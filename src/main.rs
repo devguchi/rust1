38685079a276c84b12dc1b807b38a55a71b8d3c0
devguchi <devguchitaro@gmail.com>
2021-10-07 20:38:32 +0900

    atcoder - abc176-a

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::io;
 
 fn main() {
-    let s:i64 = get_input()[0].parse().unwrap();
-    println!("{}", if s == 0 { 1 } else { 0 });
+    let nxt = get_input_i64();
+    let n = nxt[0] as f64;
+    let x = nxt[1] as f64;
+    let t = nxt[2] as f64;
+    let ans = (n/x).ceil()*t;
+    println!("{}", ans);
 }
 
-fn get_input() -> Vec<String> {
+fn get_input_i64() -> Vec<i64> {
     let mut word_line = String::new();
     io::stdin().read_line(&mut word_line).ok();
     let words: Vec<&str> = word_line.split_whitespace().collect();
-    words.iter().map(|word| word.to_string()).collect()
+    words.iter().map(|word| word.parse().unwrap()).collect()
 }
-
