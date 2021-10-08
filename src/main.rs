1dd229825aa589b4c9e82c30ad0fa9b24df981d9
devguchi <devguchitaro@gmail.com>
2021-10-08 09:47:02 +0900

    atcoder - abc175-a

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use std::io;
 
 fn main() {
-    let nxt = get_input_i64();
-    let n = nxt[0] as f64;
-    let x = nxt[1] as f64;
-    let t = nxt[2] as f64;
-    let ans = (n/x).ceil()*t;
-    println!("{}", ans);
+    let weather:Vec<char> = get_input()[0].chars().collect();
+    let mut max = 0;
+    let mut days = 0;
+    for w in weather.iter() {
+        if *w == 'R' {
+            days += 1;
+            if max < days { max = days.clone(); }
+        } else {
+            days = 0;
+        }
+    }
+    println!("{}", max);
 }
 
-fn get_input_i64() -> Vec<i64> {
+fn get_input() -> Vec<String> {
     let mut word_line = String::new();
     io::stdin().read_line(&mut word_line).ok();
     let words: Vec<&str> = word_line.split_whitespace().collect();
-    words.iter().map(|word| word.parse().unwrap()).collect()
+    words.iter().map(|word| word.to_string()).collect()
 }
+
