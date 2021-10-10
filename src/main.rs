d814147bf241ba44e79843763cad7ea26e7da15f
devguchi <devguchitaro@gmail.com>
2021-10-10 12:16:24 +0900

    atcoder - abc172-a

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::io;
 
 fn main() {
-    let n = get_input_i64()[0] as f64;
-    println!("{}", (n/1000.0).ceil()*1000.0-n);
-}
+    let n = get_input_i64()[0];
+    println!("{}", n+n.pow(2)+n.pow(3));
+} 
 
 fn get_input_i64() -> Vec<i64> {
     let mut word_line = String::new();
