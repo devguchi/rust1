8ac05c43d3a4f02b156fe214cd51d8a5416fa44f
devguchi <devguchitaro@gmail.com>
2021-10-11 00:33:27 +0900

    atcoder - arc037-c

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,31 @@
 use proconio::input;
-// use whiteread::parse_line;
 
 fn main() {
     input! {
-        n:usize,
-        mut a:[i64; n],
-        b:[i64; n],
-        mut c:[i64; n],
+        n: usize,
+        k: usize,
+        mut a: [u64; n],
+        mut b: [u64; n],
     }
     a.sort();
-    c.sort();
-    let mut ans = 0;
-    for b_idx in 0..n {
-        let a_idx = lower_bound(&a, &b[b_idx]);
-        let c_idx = upper_bound(&c, &b[b_idx]);
-        let num = a_idx*(n-c_idx);
-        ans += if num > 0 { num } else { 0 };
-    } 
-    println!("{}", ans);
-}
-
-fn lower_bound(v:&Vec<i64>, s:&i64) -> usize {
-    let mut low = 0;
-    let mut high = v.len();
-    while low != high {
-        let mid = (low + high)/2;
-        if v[mid] < *s {
-            low = mid+1;
-        } else { 
-            high = mid;
+    b.sort();
+    let mut l = 0;
+    let mut r = 1_000_000_000_000_000_000;
+    while r - l > 1 {
+        let m = (l + r) / 2;
+        let mut cnt = 0;
+        let mut j = 0;
+        for a in a.iter().rev() {
+            while j < b.len() && *a * b[j] <= m {
+                j += 1;
+            }
+            cnt += j;
+        }
+        if k <= cnt {
+            r = m;
+        } else {
+            l = m;
         }
     }
-    low
+    println!("{}", r);
 }
-
-fn upper_bound(v:&Vec<i64>, s:&i64) -> usize {
-    let mut low = 0;
-    let mut high = v.len();
-    while low != high {
-        let mid =  (low+high)/2;
-        if v[mid] > *s {
-            high = mid;
-        } else { 
-            low = mid+1;
-        }
-    }
-    low
-}
-
