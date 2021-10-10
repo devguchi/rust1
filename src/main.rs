afdbe21e7e777a7bf370bd49032511552328fd1c
devguchi <devguchitaro@gmail.com>
2021-10-10 13:22:21 +0900

    added proconio and atcoder abc-171-a

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use std::io;
+use proconio::input;
 
 fn main() {
-    let n = get_input_i64()[0];
-    println!("{}", n+n.pow(2)+n.pow(3));
-} 
-
-fn get_input_i64() -> Vec<i64> {
-    let mut word_line = String::new();
-    io::stdin().read_line(&mut word_line).ok();
-    let words: Vec<&str> = word_line.split_whitespace().collect();
-    words.iter().map(|word| word.parse().unwrap()).collect()
+    input! {
+        s:char
+    }
+    match s {
+        'A'..='Z' => println!("A"),
+        _ => println!("a")
+    }
 }
+
