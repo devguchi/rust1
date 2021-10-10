c3f95ea1e2514314633fd6627acdf9ac4f52c727
devguchi <devguchitaro@gmail.com>
2021-10-10 20:09:14 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,28 @@
-// use proconio::input;
-// use text_io::read;
-// use whiteread::parse_line;
+use proconio::input;
+use whiteread::parse_line;
 
 fn main() {
-    let mut v = vec![1, 10, 30, 2, 5, 1, 3, 3, 3, 3, 4, 230, 5, 43, 23];
-    v.sort();
-    println!("{:?}", v);
-    let s = 3;
-    // 同じ値の場合返すindexはバラバラになりうる
-    let ans1 = v.binary_search(&s);
-    println!("{:?}", ans1);
-    let ans2 = lower_bound(&v, &s);
-    println!("{:?}", ans2);
-    let ans3 = upper_bound(&v, &s);
-    println!("{:?}", ans3);
+    input! { n:usize, k:i64 }
+    let a:Vec<i64> = parse_line().unwrap(); 
+    let idx = lower_bound(&a, &k);
+    println!("{} {}", a.len(), idx);
+    if a[n-1] < k {
+        println!("-1");
+    } else { 
+        println!("{}", idx);
+    }
 }
 
-// 探している値の一番小さいindexを返す
-fn lower_bound(v:&Vec<i64>, s:&i64) -> Result<usize, usize> {
+fn lower_bound(v:&Vec<i64>, s:&i64) -> usize {
     let mut low = 0;
     let mut high = v.len();
-    let mut exist = false;
     while low != high {
         let mid = (low + high)/2;
-        match v[mid].cmp(s) {
-            std::cmp::Ordering::Less => {
-                low = mid+1;
-            },
-            std::cmp::Ordering::Greater => {
-                high = mid;
-            },
-            std::cmp::Ordering::Equal => {
-                high = mid;
-                exist = true;
-            }
+        if v[mid] > *s {
+            low = mid+1;
+        } else { 
+            high = mid;
         }
     }
-    if exist { Ok(low) } else { Err(low) }
-}
-
-// 探している値の一番大きいindexを返す
-fn upper_bound(v:&Vec<i64>, s:&i64) -> Result<usize, usize> {
-    let mut low = 0;
-    let mut high = v.len();
-    let mut exist = false;
-    while low != high {
-        let mid =  (low+high)/2;
-        match v[mid].cmp(s) {
-            std::cmp::Ordering::Greater => {
-                high = mid;
-            },
-            std::cmp::Ordering::Less => {
-                low = mid+1;
-            },
-            std::cmp::Ordering::Equal => {
-                low = mid+1;
-                exist = true;
-            }
-        }
-    }
-    if exist { Ok(low-1) } else { Err(low) }
+    low
 }
