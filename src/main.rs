858216097acdc2c40f51748d4df11eb24d795bf8
devguchi <devguchitaro@gmail.com>
2021-10-10 12:14:31 +0900

    atcoder - abc173-a

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 use std::io;
 
 fn main() {
-    let x = get_input_i64()[0];
-    if x >= 30 {
-        println!("Yes");
-    } else {
-        println!("No");
-    }
+    let n = get_input_i64()[0] as f64;
+    println!("{}", (n/1000.0).ceil()*1000.0-n);
 }
 
 fn get_input_i64() -> Vec<i64> {
