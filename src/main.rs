2ff6fa10faaad6228ec589d58f63a845f7fb0234
devguchi <devguchitaro@gmail.com>
2021-10-11 23:57:07 +0900

    atcoder - abc168-a

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        k: usize,
-        mut a: [u64; n],
-        mut b: [u64; n],
+        mut n: String,
     }
-    a.sort();
-    b.sort();
-    let mut l = 0;
-    let mut r = 1_000_000_000_000_000_000;
-    while r - l > 1 {
-        let m = (l + r) / 2;
-        let mut cnt = 0;
-        let mut j = 0;
-        for a in a.iter().rev() {
-            while j < b.len() && *a * b[j] <= m {
-                j += 1;
-            }
-            cnt += j;
-        }
-        if k <= cnt {
-            r = m;
-        } else {
-            l = m;
-        }
-    }
-    println!("{}", r);
+    let ans = match n.pop().unwrap() as i64 - 48 {
+        2 | 4 | 5 | 7 | 9 => "hon",
+        0 | 1 | 6 | 8 => "pon",
+        3 => "bon",
+        _ => "_"
+    };
+    println!("{}", ans);
 }
