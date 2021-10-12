1c6455156fb194fd1b53e7a0103536df54f224d3
devguchi <devguchitaro@gmail.com>
2021-10-13 08:08:04 +0900

    atcoder - abc167-a

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-        mut n: String,
+        s: String,
+        mut t: String,
     }
-    let ans = match n.pop().unwrap() as i64 - 48 {
-        2 | 4 | 5 | 7 | 9 => "hon",
-        0 | 1 | 6 | 8 => "pon",
-        3 => "bon",
-        _ => "_"
-    };
+    let mut ans = "No"; 
+    t.pop();
+    if s == t {
+        ans = "Yes";
+    }
     println!("{}", ans);
 }
