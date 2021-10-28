4b617972ae9cc11f620b9aa25a5e506cb552a64e
devguchi <devguchitaro@gmail.com>
2021-10-28 15:19:18 +0900

    atcoder - abc164-a

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [[f64;2]; n]
+        s: i32,
+        w: i32,
     }
-    let mut cnt = 0;
-    for i in 0..n {
-        for j in 0..n {
-            for h in 0..n {
-                if i == j || i == h || j == h {continue;}
-                let ij;
-                let ih;
-                let jh;
-                if a[i][0] == a[j][0] {
-                     ij = -1.0;
-                } else {
-                    ij = ((a[i][1]-a[j][1])/(a[i][0]-a[j][0])).abs();
-                }
-                if a[i][0] == a[h][0] { 
-                    ih = -1.0; 
-                } else {
-                    ih = ((a[i][1]-a[h][1])/(a[i][0]-a[h][0])).abs();
-                }
-                if a[j][0] == a[h][0] { 
-                    jh = -1.0; 
-                } else {
-                    jh = ((a[j][1]-a[h][1])/(a[j][0]-a[h][0])).abs();
-                }
-                if ij != ih || ij != jh || ih != jh {
-                    cnt += 1;
-                }
-            }
-        }
+    if s <= w {
+        println!("unsafe");
+    } else {
+        println!("safe");
     }
-    println!("{}", cnt/6);
 }
 
