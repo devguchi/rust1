271c79c2b21e3a6eb85d55d815dfa3030bb13871
devguchi <devguchitaro@gmail.com>
2021-11-08 21:30:20 +0900

    atcoder - abc160-a

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
 use proconio::input;
-use proconio::marker::Usize1;
+// use proconio::marker::Usize1;
 
 fn main() {
     input!{
-        n: usize,
-        a:[(u64, [Usize1]); n]
+        s: String,
     }
-    let mut ok_list: [bool; 1000000] = [false; 1000000];
-    let result = study(n-1, &a, &mut ok_list);
+    let v:Vec<char> = s.chars().collect();
+    let mut result = "No";
+    if v[2] == v[3] && v[4] == v[5] {
+        result = "Yes";
+    }
     println!("{}", result);
-}
-
-fn study(no: usize, a:&Vec<(u64, Vec<usize>)>, mut ok_list: &mut [bool; 1000000]) -> u64 {
-    if ok_list[no] { return 0 }
-    ok_list[no] = true;
-    let mut time = a[no].0;
-    if a[no].1.len() < 1 { return time }
-    for next_no in a[no].1.iter() {
-        time += study(*next_no, &a, &mut ok_list);
-    }
-    time
 }
