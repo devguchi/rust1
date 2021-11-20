de361c322ecbd1c9e8b5de1b579525750f0fa474
devguchi <devguchitaro@gmail.com>
2021-11-20 22:42:20 +0900

    atcoder - abc228-c

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 use proconio::input;
 // use proconio::marker::Usize1;
 // use proconio::marker::Chars;
-use std::collections::HashSet;
+// use std::collections::HashSet;
 
 fn main() {
     input!{
-        n: [i32; 3]
+        n: i64,
+        k: usize,
+        p_list: [(i64,i64,i64); n]
     }
-    let h:HashSet<i32> = n.into_iter().collect();
-    let mut result = "No";
-    if h.len() == 2 { result = "Yes"; }
-    println!("{}", result);
+    let mut a: Vec<i64> = vec![];
+    for p in p_list.iter() {
+        let total = p.0+p.1+p.2;
+        a.push(total);
+    }
+    a.sort_by(|x,y| y.cmp(x));
+    let limit_score = a[k-1];
+    for p in p_list.iter() {
+        let total = p.0+p.1+p.2+300;
+        if total >= limit_score {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    }
 }
