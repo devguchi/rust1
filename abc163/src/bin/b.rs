c958a34aafe8f105d1768dde1781d78361a8817c
devguchi <devguchitaro@gmail.com>
2021-11-21 23:56:14 +0900

    abc163-b,c

--- abc163/src/bin/b.rs
+++ abc163/src/bin/b.rs
@@ -2,7 +2,14 @@ use proconio::*;
 
 #[fastout]
 fn main() {
-    input!{
-        n: usize,
+    input! {
+        n: i32, m: usize,
+        a: [i32; m]
     }
+    let sum: i32 = a.iter().sum();
+    let mut ans = n - sum;
+    if ans < 0 {
+        ans = -1;
+    }
+    println!("{}", ans);
 }
--- abc163/src/bin/c.rs
+++ abc163/src/bin/c.rs
@@ -4,5 +4,13 @@ use proconio::*;
 fn main() {
     input!{
         n: usize,
+        a: [usize; n-1]
+    }
+    let mut ans_list:Vec<usize> = vec![0; n];
+    for i in 0..n-1 {
+        ans_list[a[i]-1] += 1;
+    }
+    for ans in ans_list {
+        println!("{}", ans);
     }
 }
