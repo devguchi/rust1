530c5799ddff7f7a4aa47c77c9a2b9b7adcb4ced
web3ten0 <web3ten0@gmail.com>
2021-12-20 01:31:52 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,25 @@ use proconio::*;
 
 #[fastout]
 fn main() {
-    input!{
+    input! {
         n: usize,
         m: usize,
         ab: [(usize, usize); m],
         cd: [(usize, usize); m]
     }
-    let mut a:Vec<u64> = vec![0; n];
-    let mut b:Vec<u64> = vec![0; n];
+    let mut a: Vec<u64> = vec![0; n];
+    let mut b: Vec<u64> = vec![0; n];
     for i in 0..m {
-        a[ab[i].0-1] += 1;
-        a[ab[i].1-1] += 1;
-        b[cd[i].0-1] += 1;
-        b[cd[i].1-1] += 1;
+        a[ab[i].0 - 1] += 1;
+        a[ab[i].1 - 1] += 1;
+        b[cd[i].0 - 1] += 1;
+        b[cd[i].1 - 1] += 1;
     }
     a.sort();
     b.sort();
-    let mut ans = if a == b {"Yes"} else {"No"};
-    if a > b || b > a {ans = "No";}
+    let mut ans = if a == b { "Yes" } else { "No" };
+    if a > b || b > a {
+        ans = "No";
+    }
     println!("{}", ans);
 }
