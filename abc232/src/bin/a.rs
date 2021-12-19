ad871c2ab01573f21466280dd7be893f3c1c4a5f
web3ten0 <web3ten0@gmail.com>
2021-12-19 22:40:50 +0900

    abc232-a,b

--- abc232/src/bin/a.rs
+++ abc232/src/bin/a.rs
@@ -1,8 +1,11 @@
 use proconio::*;
+use proconio::marker::Chars;
 
 #[fastout]
 fn main() {
-    input!{
-        n: usize,
-    }
+    input!{ s: Chars }
+    let a:u32 = s[0] as u32 - '0' as u32;
+    let b:u32 = s[2] as u32 - '0' as u32;
+    println!("{}", a*b);
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,24 @@
 use proconio::*;
-// use proconio::marker::{Chars};
-// use std::collections::HashSet;
 
 #[fastout]
 fn main() {
-    input! {
-        n: usize, k:usize,
-        mut p: [usize; n]
+    input!{
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m],
+        cd: [(usize, usize); m]
     }
-    let mut max_sum = 0;
-    let mut max_idx = 0;
-    for i in 0..n {
-        if i + k > n { break; }
-        let sum: usize = p[i..i + k].iter().sum();
-        if sum > max_sum {max_sum = sum; max_idx = i;}
+    let mut a:Vec<u64> = vec![0; n];
+    let mut b:Vec<u64> = vec![0; n];
+    for i in 0..m {
+        a[ab[i].0-1] += 1;
+        a[ab[i].1-1] += 1;
+        b[cd[i].0-1] += 1;
+        b[cd[i].1-1] += 1;
     }
-
-    let mut ans = 0.0;
-    for x in p[max_idx..max_idx+k].iter() {
-        for y in 1..x+1 {
-            ans += y as f64 / *x as f64;
-        }
-    }
+    a.sort();
+    b.sort();
+    let mut ans = if a == b {"Yes"} else {"No"};
+    if a > b || b > a {ans = "No";}
     println!("{}", ans);
 }
