a456b71e88df7ca1a11c2de46eac94cfd48e055e
web3ten0 <web3ten0@gmail.com>
2023-06-13 23:15:39 +0900

    abc284-a

---
+++ abc284/src/bin/a.rs
@@ -0,0 +1,18 @@
+#![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
+use itertools::{iproduct, Itertools};
+use petgraph::{algo::is_isomorphic, graph::UnGraph};
+use proconio::marker::{Bytes, Chars, Usize1};
+use proconio::*;
+use std::cmp::{max, min, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        strings: [String; n]
+    }
+    for s in strings.into_iter().rev() {
+        println!("{}", s);
+    }
+}
